884db3f008a6a5d2facb01607ee0ffe86334dde9
Houssem Dellai <houssem.dellai@live.com>
2020-12-31 16:30:12 +0100

    created stage 3

---
+++ stage3/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate0105"
+    container_name       = "tfstate"
+    key                  = "stage3.terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "stage1_state" {
+  backend = "azurerm"
+  config = {
+    storage_account_name = "tfstate0105"
+    container_name       = "tfstate"
+    key                  = "stage1.terraform.tfstate"
+  }
+}
