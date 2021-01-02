047b487ed58d52879be8acd81b1911e85262652a
Houssem Dellai <houssem.dellai@live.com>
2021-01-02 10:02:26 +0100

    edited main.tf

--- stage4/providers.tf
+++ stage4/providers.tf
@@ -1,9 +1,9 @@
 provider "helm" {
   kubernetes {
     host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
   }
 }
 
